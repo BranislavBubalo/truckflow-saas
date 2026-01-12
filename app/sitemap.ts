959c104c265d759229e94806244ea8e101647fa5
Branislav <bgdland@gmail.com>
2026-01-12 16:35:41 +0100

    Update sitemap with all pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,14 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://fleetexpedite.com',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
+      changeFrequency: 'weekly',
+      priority: 1.0,
     },
     {
       url: 'https://fleetexpedite.com/owner-operator-app',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.9,
+    },
+    {
+      url: 'https://fleetexpedite.com/driver-app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://fleetexpedite.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
     },
     {
       url: 'https://fleetexpedite.com/login',
